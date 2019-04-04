3440892cbb1d763ff3869497ca8da9cc56b043c0
rainer <rainer@systemkern.com>
2019-04-04 15:33:43 +0200

    Add more text to message

--- build/index.js
+++ build/index.js
@@ -5,17 +5,9 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!';
-  res.status(200).send(message);
-};
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello Javascript World!';
+    let message = req.query.message
+        || req.body.message
+        || 'Hello Javascript World! Pushed from Github';
   res.status(200).send(message);
 };
 
