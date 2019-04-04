7c20796825883377b23909e167a27b344515b42f
rainer <rainer@systemkern.com>
2019-04-04 19:28:38 +0200

    Add Kotlin

--- web/index.js
+++ web/index.js
@@ -2,7 +2,7 @@
 // const testModule = require('./systekern-gcp-function-test-project');
 const fs = require('fs');
 const kotlin = require('./kotlin.js');
-// const custom = require('./systemkern-gcp-test-project.js');
+const custom = require('./systemkern-gcp-test-project.js');
 const sayHi = require('./sayHi.js');
 
 /**
@@ -18,8 +18,9 @@ exports.helloWorld = (req, res) => {
             'req.query.message: ' + req.query.message + '<br>' +
             'req.body.message: ' + req.body.message + '<br>' +
             'files: ' + files + '<br/>' +
-            'sayHi: ' + sayHi() + '<br/>';
-//            'kotlin Hello: ' + _.hello() + '<br/>';
+            'sayHi: ' + sayHi() + '<br/> +' +
+            'Kotlin: <br/> +';
+//           'kotlin Hello: ' + _.hello() + '<br/>';
 
         res.status(200).send(message);
     });
