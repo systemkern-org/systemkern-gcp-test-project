b1515fe07636c745e37bc2ddbcfef6050a9595e7
rainer <rainer@systemkern.com>
2019-04-04 17:47:05 +0200

    Improve function

--- web/index.js
+++ web/index.js
@@ -8,9 +8,10 @@ const functions = require('./systekern-gcp-function-test-project');
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
-    let message = req.query.message
-        || req.body.message
-        || 'Hello Javascript World! Pushed from Github';
+    let message = 'Hello Javascript World! Pushed from Github<br>' +
+        'Hopefully with Kotlin ;-)<br>' +
+        'req.query.message: ' + req.query.message + '<br>' +
+        'req.body.message:' + req.body.message + '<br>';
     res.status(200).send(message);
 };
 
