1bf0664e3160bbf61dbe9c21cd10e1f827f42dea
rainer <rainer@systemkern.com>
2019-04-04 19:12:22 +0200

    Add Kotlin again

--- web/index.js
+++ web/index.js
@@ -1,6 +1,7 @@
 // const kotlin = require('./kotlin');
 // const testModule = require('./systekern-gcp-function-test-project');
 const fs = require('fs');
+const kotlin = require('./kotlin.js');
 const sayHi = require('./sayHi.js');
 
 /**
@@ -11,7 +12,7 @@ const sayHi = require('./sayHi.js');
  */
 exports.helloWorld = (req, res) => {
     fs.readdir(__dirname, (err, files) => {
-        let message = 'Hello Javascript World! Pushed from Github<br>' +
+        let message = 'Hello Javascript World!<br>Pushed from Github<br>' +
             'Hopefully with Kotlin ;-)<br>' +
             'req.query.message: ' + req.query.message + '<br>' +
             'req.body.message: ' + req.body.message + '<br>' +
