3fd29e091aab6add09212ff5b17345b26a0efc91
rainer <rainer@systemkern.com>
2019-04-04 19:01:01 +0200

    Add node module

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
 // const kotlin = require('./kotlin');
 // const testModule = require('./systekern-gcp-function-test-project');
-const foo = require('./foo');
+const fs = require('fs');
 
 /**
  * Responds to any HTTP request.
@@ -9,12 +9,23 @@ const foo = require('./foo');
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
+    fs.readdir(__dirname, (err, files) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(500);
+        } else {
+            console.log('Files', files);
+            res.sendStatus(200);
+        }
+    });
+
     let message = 'Hello Javascript World! Pushed from Github<br>' +
         'Hopefully with Kotlin ;-)<br>' +
         'req.query.message: ' + req.query.message + '<br>' +
-        'req.body.message: ' + req.body.message + '<br>' +
-        'foo: ' + foo() + '<br>';
-//        'kotlin hello: ' + hello() + '<br>';
+        'req.body.message: ' + req.body.message + '<br>';
+
     res.status(200).send(message);
+    //        'foo: ' + foo() + '<br>';
+//        'kotlin hello: ' + hello() + '<br>';
 };
 
