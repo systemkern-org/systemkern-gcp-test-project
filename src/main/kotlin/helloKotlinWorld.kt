72180a702627ad4475b1572a6398b8dccf643097
rainer <rainer@systemkern.com>
2019-04-04 22:56:35 +0200

    Convert all code to Kotlin

--- src/main/kotlin/helloKotlinWorld.kt
+++ src/main/kotlin/helloKotlinWorld.kt
@@ -5,12 +5,13 @@ fun hello() = "Hello Kotlin World"
 
 @JsName("processRequest")
 fun processRequest(req: dynamic, res: dynamic) {
+    val query: String = req.query.message as String
+    val body: String = req.body.message as String
     val message = """
         |Pushed from Github<br>
-        |req.query.message: ${req.query.message}<br>
-        |req.body.message: ${req.body.message}<br>
+        |req.query.message: $query<br>
+        |req.body.message: $body<br>
     """.trimMargin()
 
     res.status(200).send(message)
-    return
 }
