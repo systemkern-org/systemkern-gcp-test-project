65fc2affc99744e467ed5a27df9032457e3f7d5e
rainer <rainer@systemkern.com>
2019-04-04 19:33:13 +0200

    Add Kotlin3

--- web/index.js
+++ web/index.js
@@ -1,7 +1,7 @@
 // const kotlin = require('./kotlin');
 // const testModule = require('./systekern-gcp-function-test-project');
 const fs = require('fs');
-const kotlin = require('./kotlin.js');
+//const kotlin = require('./kotlin.js');
 //const custom = require('./systemkern-gcp-test-project.js');
 const sayHi = require('./sayHi.js');
 
