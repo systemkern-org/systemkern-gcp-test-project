cd5906534adf9889a0695a0a12345fe5e197443f
rainer <rainer@systemkern.com>
2019-04-04 22:51:53 +0200

    Convert all code to Kotlin

--- web/index.js
+++ web/index.js
@@ -2,7 +2,7 @@ const kotlin = require('./kotlin.js');
 const custom = require('./systemkern-gcp-test-project.js');
 const jsFunc = require('./function.js');
 
-//hello world endpoint
+//hello world endpoint this has to be correlated with cloudbuild.yml
 exports.helloWorld = (req, res) => {
     custom.processRequest(req, res);
 };
