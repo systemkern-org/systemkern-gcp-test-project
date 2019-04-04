158269d5da26d50894aa00b76e79b7b4bdcb6d12
rainer <rainer@systemkern.com>
2019-04-04 19:40:26 +0200

    Add Kotlin7

--- web/index.js
+++ web/index.js
@@ -1,8 +1,7 @@
 // const kotlin = require('./kotlin');
 // const testModule = require('./systekern-gcp-function-test-project');
-const fs = require('fs');
-const kotlin = require('./kotlin.js');
-const custom = require('./systemkern-gcp-test-project.js');
+// const kotlin = require('./kotlin.js');
+// const custom = require('./systemkern-gcp-test-project.js');
 const sayHi = require('./sayHi.js');
 
 /**
@@ -12,18 +11,14 @@ const sayHi = require('./sayHi.js');
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
-    fs.readdir(__dirname, (err, files) => {
+    let message = 'Hello Javascript World!<br>Pushed from Github<br>' +
+        'Hopefully with Kotlin ;-)<br>' +
+        'req.query.message: ' + req.query.message + '<br>' +
+        'req.body.message: ' + req.body.message + '<br>' +
+        'sayHi: ' + sayHi() + '<br/>';
+    // 'Kotlin: ' + kot + '<br/>';
+    // 'kotlin Hello: ' + _.hello() + '<br/>';
 
-        let message = 'Hello Javascript World!<br>Pushed from Github<br>' +
-            'Hopefully with Kotlin ;-)<br>' +
-            'req.query.message: ' + req.query.message + '<br>' +
-            'req.body.message: ' + req.body.message + '<br>' +
-            'files: ' + files + '<br/>' +
-            'sayHi: ' + sayHi() + '<br/>';
-        // 'Kotlin: ' + kot + '<br/>';
-        // 'kotlin Hello: ' + _.hello() + '<br/>';
-
-        res.status(200).send(message);
-    });
+    res.status(200).send(message);
 };
 
