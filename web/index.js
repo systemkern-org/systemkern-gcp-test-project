3a84437db9b8bd4dce6c0c4858a35d37684d6587
rainer <rainer@systemkern.com>
2019-04-04 19:31:10 +0200

    Add Kotlin2

--- web/index.js
+++ web/index.js
@@ -2,7 +2,7 @@
 // const testModule = require('./systekern-gcp-function-test-project');
 const fs = require('fs');
 const kotlin = require('./kotlin.js');
-const custom = require('./systemkern-gcp-test-project.js');
+//const custom = require('./systemkern-gcp-test-project.js');
 const sayHi = require('./sayHi.js');
 
 /**
@@ -13,14 +13,21 @@ const sayHi = require('./sayHi.js');
  */
 exports.helloWorld = (req, res) => {
     fs.readdir(__dirname, (err, files) => {
+        var kot = "";
+        try {
+            //       kot = hello()
+        } catch (e) {
+            kot = "Error"
+        }
+
         let message = 'Hello Javascript World!<br>Pushed from Github<br>' +
             'Hopefully with Kotlin ;-)<br>' +
             'req.query.message: ' + req.query.message + '<br>' +
             'req.body.message: ' + req.body.message + '<br>' +
             'files: ' + files + '<br/>' +
             'sayHi: ' + sayHi() + '<br/> +' +
-            'Kotlin: <br/> +';
-//           'kotlin Hello: ' + _.hello() + '<br/>';
+            //            'Kotlin: ' + kot + '<br/>';
+            //            'kotlin Hello: ' + _.hello() + '<br/>';
 
         res.status(200).send(message);
     });
