2f857d2a2b962746459c4705b71ded93f964dbbf
rainer <rainer@systemkern.com>
2019-04-05 08:20:19 +0200

    Readd JS response

--- src/main/javascript/index.js
+++ src/main/javascript/index.js
@@ -8,7 +8,22 @@ exports.procRequest = (req, res) => {
 };
 
 exports.procBody = (req, res) => {
+    let message = 'Hello Javascript World!<br>Pushed from Github<br>' +
+        'Not yet with Kotlin :-(<br>' +
+        'But added the Includes:-(<br>' +
+        'req.query.message: ' + req.query.message + '<br>' +
+        'req.body.message: ' + req.body.message + '<br>' +
+        'sayHi: ' + sayHi() + '<br/>';
+    // 'Kotlin: ' + kot + '<br/>';
+    // 'kotlin Hello: ' + _.hello() + '<br/>';
+
+
     let response = custom.processBody(req.body.message);
-    res.status(response.code).send(response.message)
+    res.status(response.code).send(
+        '<hr>Javascript Response:' + '<br>' +
+        message + '<br>' +
+        '<hr>Kotlin Response:' + '<br>' +
+        response.message
+    )
 };
 
