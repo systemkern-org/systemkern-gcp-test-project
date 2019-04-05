e6e93ec59423e32deb9358e97252d6b46f278095
rainer <rainer@systemkern.com>
2019-04-05 08:25:40 +0200

    Readd JS response

--- src/main/javascript/index.js
+++ src/main/javascript/index.js
@@ -4,6 +4,15 @@ const jsFunc = require('./function.js');
 
 //endpoint this has to be correlated with cloudbuild.yml
 exports.procRequest = (req, res) => {
+    let message = 'Hello Javascript World!<br>Pushed from Github<br>' +
+        'Not yet with Kotlin :-(<br>' +
+        'But added the Includes:-(<br>' +
+        'req.query.message: ' + req.query.message + '<br>' +
+        'req.body.message: ' + req.body.message + '<br>' +
+        'sayHi: ' + sayHi() + '<br/>';
+
+    res.status(200).send(message);
+
     custom.processRequest(req, res);
 };
 
@@ -19,7 +28,7 @@ exports.procBody = (req, res) => {
 
 
     let response = custom.processBody(req.body.message);
-    res.status(response.code).send(
+    res.status(200).send(
         '<hr>Javascript Response:' + '<br>' +
         message + '<br>' +
         '<hr>Kotlin Response:' + '<br>' +
